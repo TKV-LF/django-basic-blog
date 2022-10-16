0e4f6dc34b1b42bab98101e524e1006065ca4009
ThuyNT <thuyngtrong@gmail.com>
2022-10-17 01:16:56 +0700

    sf

--- blango/urls.py
+++ blango/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path("", blog.views.index),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
 
 ]
